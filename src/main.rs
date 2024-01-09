1600f8a0a616ec18470313883472fa39ebea748f
0xflotus <0xflotus@gmail.com>
2024-01-09 13:45:35 +0100

    v0.4.0

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
-use clap::{Command, Arg};
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
     let matches = Command::new("ip2d")
         .author("0xflotus")
-        .version("0.3.0")
+        .version("0.4.0")
         .about("A converter for IPv4 Addresses")
         .arg(
             Arg::new("ip")
                 .help("Converts an IPv4 Address to an integer")
-                .required(false)
-                .index(1_usize),
         )
         .arg(
             Arg::new("reverse")
                 .short('r')
                 .long("reverse")
-                .takes_value(true)
                 .value_name("number")
                 .help("Converts an integer to an IPv4 Address"),
         )
@@ -23,30 +20,28 @@ fn main() {
             Arg::new("hex")
                 .short('x')
                 .long("hex")
-                .takes_value(false)
+                .action(ArgAction::SetTrue)
                 .help("Converts an IPv4 Address to a hex number"),
         )
         .get_matches();
 
-    if matches.is_present("reverse") {
-        if let Some(number) = matches.value_of("reverse") {
-            let num = number.parse::<u32>().unwrap();
-            let vec: Vec<String> = vec![0x18, 0x10, 0x8, 0x0]
-                .into_iter()
-                .map(|x| (num >> x) & 0xff)
-                .map(|x| x.to_string())
-                .collect();
-            println!("{}", vec.join("."));
-        }
+    if let Some(number) = matches.get_one::<String>("reverse") {
+        let num = number.parse::<u32>().unwrap();
+        let vec: Vec<String> = vec![0x18, 0x10, 0x8, 0x0]
+            .into_iter()
+            .map(|x| (num >> x) & 0xff)
+            .map(|x| x.to_string())
+            .collect();
+        println!("{}", vec.join("."));
         return;
     }
 
-    if let Some(ip) = matches.value_of("ip") {
+    if let Some(ip) = matches.get_one::<String>("ip") {
         let splitted = ip
             .split(".")
             .map(|x| x.parse::<u32>().unwrap())
             .fold(0, |x, y| (x << 0x8) | y);
-        if matches.is_present("hex") {
+        if matches.get_flag("hex") {
             println!("{:#010x}", splitted);
         } else {
             println!("{}", splitted);
