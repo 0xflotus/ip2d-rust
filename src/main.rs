829302ef59e85e4b90ed0930a4c31cef9ac4cec6
0xflotus <0xflotus@gmail.com>
2024-01-12 10:04:21 +0100

    v0.4.2

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::{Arg, ArgAction, Command};
 fn main() {
     let matches = Command::new("ip2d")
         .author("0xflotus")
-        .version("0.4.1")
+        .version("0.4.2")
         .about("A converter for IPv4 Addresses")
         .arg(
             Arg::new("ip")
