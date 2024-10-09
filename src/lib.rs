1c3a6ebc9c7b872188eacb02aa0fd27f4de2f01b
Garrett Squire <github@garrettsquire.com>
2024-10-09 11:03:17 -0700

    Add IPv6 Support
    This patch adds IPv6 support. I also added the anyhow crate for error
    handling.

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,76 @@
-pub fn to_u32(ip: &str) -> u32 {
-    ip.split(".")
-        .map(|x| x.parse::<u32>().unwrap())
-        .fold(0, |x, y| (x << 0x8) | y)
+use std::net::IpAddr::{self, *};
+use std::net::Ipv6Addr;
+
+use anyhow::Result;
+
+pub fn to_integer(ip: &str) -> Result<u128> {
+    let addr: IpAddr = ip.parse()?;
+    match addr {
+        V4(a) => {
+            let res: u32 = a.into();
+            Ok(res as u128)
+        }
+        V6(a) => Ok(a.into()),
+    }
 }
 
-pub fn to_str(num: u32) -> String {
-    let vec: Vec<String> = vec![0x18, 0x10, 0x8, 0x0]
-            .into_iter()
-            .map(|x| ((num >> x) & 0xff).to_string())
-            .collect();
-    return vec.join(".");
+pub fn to_str(num: u128) -> String {
+    let addr = Ipv6Addr::from(num);
+    // Try and see if we can return an IPv4 address, otherwise fallback to IPv6.
+    match addr.to_ipv4() {
+        Some(a) => a.to_string(),
+        None => addr.to_string(),
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{to_u32, to_str};
+    use super::{to_integer, to_str};
 
     #[test]
-    fn u32_works_with_255_0_255_0() {
-        let result = to_u32("255.0.255.0");
+    fn to_integer_works_with_255_0_255_0() {
+        let result = to_integer("255.0.255.0").unwrap();
         assert_eq!(result, 4278255360);
     }
 
     #[test]
-    fn u32_works_with_0_0_0_1() {
-        let result = to_u32("0.0.0.1");
+    fn to_integer_works_with_0_0_0_1() {
+        let result = to_integer("0.0.0.1").unwrap();
         assert_eq!(result, 1);
     }
 
     #[test]
-    fn u32_works_with_255_255_255_255() {
-        let result = to_u32("255.255.255.255");
+    fn to_integer_works_with_255_255_255_255() {
+        let result = to_integer("255.255.255.255").unwrap();
         assert_eq!(result, 0xFFFFFFFF);
+    }
+
+    #[test]
+    fn to_integer_works_with_ipv6() {
+        let cases = vec!["::1", "2001:4860:4860::8888", "2606:4700:4700::1111"];
+        let expected = vec![
+            1,
+            42541956123769884636017138956568135816,
+            50543257694033307102031451402929180945,
+        ];
+        for (i, case) in cases.iter().enumerate() {
+            let result = to_integer(case).unwrap();
+            assert_eq!(result, expected[i]);
+        }
+    }
+
+    #[test]
+    fn to_integer_parse_failure() {
+        let cases = vec!["256.0.0.0", "1::::1"];
+        for case in cases {
+            assert!(to_integer(case).is_err());
+        }
+    }
+
+    #[test]
+    fn str_works_with_ipv6() {
+        let result = to_str(50543257694033307102031451402929180945);
+        assert_eq!(result, "2606:4700:4700::1111");
     }
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,48 @@
+use anyhow::Result;
 use clap::{Arg, ArgAction, Command};
-use ip2d::{to_u32, to_str};
+use ip2d::{to_integer, to_str};
 
-fn main() {
+fn main() -> Result<()> {
     let matches = Command::new("ip2d")
         .author("0xflotus")
         .version("0.5.0")
-        .about("A converter for IPv4 Addresses")
-        .arg(Arg::new("ip").help("Converts an IPv4 Address to an integer"))
+        .about("A converter for IP Addresses")
+        .arg(Arg::new("ip").help("Converts an IP Address to an integer"))
         .arg(
             Arg::new("reverse")
                 .short('r')
                 .long("reverse")
                 .value_name("number")
-                .help("Converts an integer to an IPv4 Address"),
+                .help("Converts an integer to an IP Address"),
         )
         .arg(
             Arg::new("hex")
                 .short('x')
                 .long("hex")
                 .action(ArgAction::SetTrue)
-                .help("Converts an IPv4 Address to a hex number"),
+                .help("Converts an IP Address to a hex number"),
         )
         .get_matches();
 
     if let Some(number) = matches.get_one::<String>("reverse") {
-        let num = number.parse::<u32>().unwrap();
+        let num = number.parse::<u128>()?;
         println!("{}", to_str(num));
-        return;
+        return Ok(());
     }
 
     if let Some(ip) = matches.get_one::<String>("ip") {
-        let splitted = to_u32(ip);
-        if matches.get_flag("hex") {
-            println!("{:#010x}", splitted);
+        if let Ok(splitted) = to_integer(ip) {
+            if matches.get_flag("hex") {
+                println!("{:#010x}", splitted);
+            } else {
+                println!("{}", splitted);
+            }
         } else {
-            println!("{}", splitted);
+            println!("An invalid IP was provided.")
         }
     } else {
-        println!("Please specify an IPv4 address to convert.");
+        println!("Please specify an IP address to convert.");
     }
+
+    Ok(())
 }
